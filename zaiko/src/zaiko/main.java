d2000751abd118421f37e21bc190a594f1326773
axdevil <deb.devel@outlook.es>
2023-01-02 18:45:33 -0600

    Conexion a base de datos

--- zaiko/src/zaiko/main.java
+++ zaiko/src/zaiko/main.java
@@ -1,5 +1,7 @@
 package zaiko;
 
+import database.connection;
+
 /**
  *
  * @author jorge
@@ -10,7 +12,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        connection.connection();
     }
     
 }
