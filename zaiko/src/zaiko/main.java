676958ee2bbff08a5f8cdbea354f003d0533a73c
axdevil <deb.devel@outlook.es>
2023-01-03 11:47:11 -0600

    Paneles del menu y tema

--- zaiko/src/zaiko/main.java
+++ zaiko/src/zaiko/main.java
@@ -1,6 +1,8 @@
 package zaiko;
 
-import database.connection;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import views.menu;
 
 /**
  *
@@ -12,7 +14,15 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        connection.connection();
+        try{
+            UIManager.setLookAndFeel(new com.formdev.flatlaf.intellijthemes.FlatArcDarkIJTheme());
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "No se pudo cargar el tema" + e, "Error", 0);
+        }
+        
+        menu w = new menu();
+        w.setLocationRelativeTo(null);
+        w.setVisible(true);
     }
     
 }
